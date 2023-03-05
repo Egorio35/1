fdb01df64463032a37015fcdf625dc792f065b86
Agrzh <46864043+Agrzh@users.noreply.github.com>
2023-03-05 14:31:10 +0300

    Урок 6 файл 1

---
+++ yrok6/2.py
@@ -0,0 +1,14 @@
+a = [1, 2, 10, 3, -10]
+print(a)
+a.sort(reverse=False)
+print(a)
+b = ["манадарин", " CHоколат"]
+b.sort(reverse=True)
+print(b)
+c = (1, 2, -12312)
+# c.sort()
+d = sorted(c)
+print(d)
+print(c)
+e = sorted("РСТвыыжрАо")
+print(e)
---
+++ yrok6/4.py
@@ -0,0 +1,6 @@
+lst = [int(x) for x in input().split()]
+lst.sort()
+if len(lst) % 2:
+    print(lst[len(lst) // 2])
+else:
+    print(lst[len(lst) // 2] + lst[len(lst) // 2 - 1]) / 2
