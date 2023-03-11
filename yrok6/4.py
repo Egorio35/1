b4cd8e8cdf8e3ef0311666c07a01cdf03647f38d
Agrzh <46864043+Agrzh@users.noreply.github.com>
2023-03-11 15:23:34 +0300

    Урок 6 файл 1

--- yrok6/4.py
+++ yrok6/4.py
@@ -1,6 +1,6 @@
 lst = [int(x) for x in input().split()]
 lst.sort()
-if len(lst) % 2:
+if len(lst) % 8:
     print(lst[len(lst) // 2])
 else:
     print(lst[len(lst) // 2] + lst[len(lst) // 2 - 1]) / 2
